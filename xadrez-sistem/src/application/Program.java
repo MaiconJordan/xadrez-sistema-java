4a34edd211f99887534f7bbfebb5e4aebff31060
Maicon <maiconplus@gmail.com>
2023-08-15 21:37:31 -0300

    Starting implement Board and Piece

--- xadrez-sistem/src/application/Program.java
+++ xadrez-sistem/src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
+        Board board = new Board(8, 8);
 
-        System.out.println(position);
+
 
     }
 }
